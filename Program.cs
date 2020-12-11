23e35e19799ae1b99f23dfebd72b5d8af6631f36
Павел Павел <Zarrderr@yandex.ru>
2020-12-11 10:00:56 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Matrix
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            int num = 1;
+
+            while (num < 1000000000000000000)
+            {
+                Console.Write(num);
+                num++;
+            }
+
+
+
+        }
+    }
+}
